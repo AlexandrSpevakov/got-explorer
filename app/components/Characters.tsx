5176712c10a4a38a8d39e933e0fb670a2fc04c88
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-18 05:14:24 -0400

    refactor: improve Characters section

--- app/components/Characters.tsx
+++ app/components/Characters.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import nedStark from '../../public/home/nedStark.webp';
-import bg from '../../public/home/faces.jpeg';
+import charactersImg from '../../public/home/charactersImg.webp';
+import charactersBg from '../../public/home/charactersBg.jpeg';
 
 export default function Characters() {
   return (
-    <article id="houses">
+    <article id="characters">
       <section className="mx-auto w-11/12">
         <h2 className="pt-20 text-center font-cardo text-[84px] 2xl:text-8xl">
           Characters
         </h2>
         <section className="mt-0 2xl:mt-12">
           <Image
-            src={nedStark}
+            src={charactersImg}
             alt="Ned Stark"
             className="float-right ml-5 w-4/12 rounded-md 2xl:w-2/12"
           />
@@ -60,7 +60,7 @@ export default function Characters() {
         <div
           className="absolute top-0 left-0 h-full w-full bg-cover bg-center bg-no-repeat opacity-15 duration-700 group-hover:opacity-10"
           style={{
-            backgroundImage: `url(${bg.src})`,
+            backgroundImage: `url(${charactersBg.src})`,
           }}
         />
         <h3 className="absolute top-1/3 right-1/2 w-4/6 translate-x-1/2 text-center font-cardo text-7xl opacity-100 duration-500 group-hover:text-orange-600 2xl:text-8xl">
