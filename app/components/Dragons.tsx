db32346515945635ed1ba95639c996c391f3f4e1
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-18 05:42:28 -0400

    refactor: adjust the main text about dragons

--- app/components/Dragons.tsx
+++ app/components/Dragons.tsx
@@ -1,28 +1,30 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import dragon from '../../public/home/dragon.webp';
-import bg from '../../public/home/dragons.webp';
+import dragonsImg from '../../public/home/dragonsImg.webp';
+import dragonsBg from '../../public/home/dragonsBg.webp';
 
 export default function Dragons() {
   return (
-    <article>
-      <section id="houses" className="mx-auto w-11/12">
-        <h2 className="pt-20 text-center font-cardo text-[84px] 2xl:text-8xl">
+    <article id="dragons">
+      <section className="mx-auto w-11/12">
+        <h2 className="pt-18 text-center font-cardo text-[84px] 2xl:text-8xl">
           Dragons
         </h2>
-        <section className="mt-0 2xl:mt-12">
+        <section className="mt-8 2xl:mt-12">
           <Image
-            src={dragon}
+            src={dragonsImg}
             alt="dragon"
-            className="float-right ml-5 w-4/12 rounded-md 2xl:w-2/12"
+            className="float-right ml-5 mb-3 w-4/12 rounded-md 2xl:w-2/12"
           />
-          <p className="mt-8 text-3xl">
-            In the world of ice and fire, dragons are a mythical and powerful
-            species that once roamed the land and skies of Westeros and Essos.
-            These creatures are massive, winged reptiles with long, sharp teeth
-            and claws, and can breathe fire, making them a fearsome and
-            formidable foe.
+          <p className="text-3xl">
+            Dragons, the powerful mythical beasts of the world of ice and fire,
+            were massive winged reptiles that once roamed the skies and lands of
+            Westeros and Essos, feared and revered for their sharp teeth, claws,
+            and fire-breathing abilities, inspiring awe-inspiring legends of
+            dragon-riders who tamed and rode these fearsome creatures, and still
+            capturing the imagination of many who wonder if they may still exist
+            in some distant corner of the world.
           </p>{' '}
           <p className="mt-8 text-3xl">
             The dragons of this world are intelligent, and can form strong bonds
@@ -38,21 +40,23 @@ export default function Dragons() {
             known dragons died out many years before the events of the series.
           </p>{' '}
           <p className="mt-4 text-2xl">
-            Despite their power, dragons are not infallible. They can be tamed
-            and trained, but they are still wild beasts with a dangerous and
-            unpredictable nature. As such, their use in battle and warfare can
-            be both a great advantage and a great risk.
+            While dragons can be tamed and trained, they remain unpredictable
+            wild beasts with a dangerous nature. Although their formidable
+            abilities can be a great asset in battle, it also poses great risks.
+            Dragon riders must have the knowledge and skill to command these
+            creatures and withstand their fiery temperament, or face
+            catastrophic consequences.
           </p>
         </section>
       </section>
       <Link
         href="/dragons"
-        className="group relative mt-28 flex h-96 w-screen items-center justify-center bg-black 2xl:mt-44"
+        className="group relative mt-8 flex h-96 w-screen items-center justify-center bg-black 2xl:mt-44"
       >
         <div
           className="absolute top-0 left-0 h-full w-full bg-cover bg-center bg-no-repeat opacity-30 duration-700 group-hover:opacity-15"
           style={{
-            backgroundImage: `url(${bg.src})`,
+            backgroundImage: `url(${dragonsBg.src})`,
           }}
         />
         <h3 className="absolute top-1/3 right-1/2 w-4/6 translate-x-1/2 text-center font-cardo text-7xl opacity-100 duration-500 group-hover:text-orange-600 2xl:text-8xl">
