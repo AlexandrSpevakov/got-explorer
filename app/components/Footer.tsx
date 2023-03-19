c7095bbfe360d385d2bd87a21dc831ec9a722b43
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-19 07:58:30 -0400

    feat: implement footer on sm size

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,24 +3,24 @@ import { BsGithub, BsLinkedin, BsInstagram } from 'react-icons/bs';
 export default function Footer() {
   return (
     <footer className="w-screen bg-black shadow-xl">
-      <ul className="mx-auto flex w-11/12 justify-around py-12 text-base md:py-20 md:text-xl 2xl:py-24 2xl:text-2xl">
+      <ul className="mx-auto flex w-11/12 flex-col justify-around gap-y-5 py-7 text-base sm:flex-row sm:py-12 md:py-20 md:text-xl 2xl:py-24 2xl:text-2xl">
         <li>
           <a
             href="mailto:spievakov@gamil.com"
-            className="duration-300 hover:text-orange-600"
+            className="block text-center duration-300 hover:text-orange-600"
           >
-            <span>spievakov@gmail.com</span>
+            spievakov@gmail.com
           </a>
         </li>
         <li>
           <a
             href="tel:+16474739502"
-            className="duration-300 hover:text-orange-600"
+            className="block text-center duration-300 hover:text-orange-600"
           >
-            <span>+1 (647) 473-9502 </span>
+            +1 (647) 473-9502
           </a>
         </li>
-        <li className="flex gap-x-8 text-2xl md:text-3xl lg:gap-x-[3.35rem] 2xl:text-4xl">
+        <li className="mx-auto flex gap-x-8 text-xl sm:text-2xl md:text-3xl lg:gap-x-[3.35rem] 2xl:text-4xl">
           <a
             href="https://github.com/AlexandrSpevakov/got-explorer"
             target="_blank"
