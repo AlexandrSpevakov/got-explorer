70d5f2afab057fcf7da99c20c6cffd73dc592e64
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-19 06:02:17 -0400

    feat: improve home hero on md size

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,20 +4,20 @@ import bg from '../../public/home/heroBg.jpg';
 
 export default function Hero() {
   return (
-    <section className="relative h-screen w-screen">
-      <div
-        className="absolute top-0 left-0 h-screen w-screen bg-cover bg-center bg-no-repeat opacity-50"
-        style={{
-          backgroundImage: `url(${bg.src})`,
-        }}
-      />
-      <h2 className="absolute top-1/4 right-1/2 w-11/12 translate-x-1/2 text-center font-cardo text-6xl md:w-5/6 md:text-7xl lg:w-4/6 lg:text-7.5xl xl:text-8xl 2xl:text-8.5xl">
+    <section
+      className="relative top-0 left-0 w-screen bg-cover bg-center bg-no-repeat after:absolute after:h-full after:w-full after:bg-neutral-900 after:opacity-50 md:h-screen"
+      style={{
+        backgroundImage: `url(${bg.src})`,
+      }}
+      id="homeHero"
+    >
+      <h2 className="absolute top-1/4 right-1/2 z-10 w-11/12 translate-x-1/2 text-center font-cardo text-6xl md:w-5/6 md:text-7xl lg:w-4/6 lg:text-7.5xl xl:text-8xl 2xl:w-3/5 2xl:text-8.5xl">
         Explore The World of Ice & Fire
       </h2>
 
       <a
         href="#houses"
-        className="absolute top-8.5/10 right-1/2 translate-x-1/2 text-center text-7xl duration-300 hover:text-orange-600 2xl:text-7.5xl"
+        className="absolute top-8.5/10 right-1/2 z-10 translate-x-1/2 text-center text-7xl duration-300 hover:text-orange-600 2xl:text-7.5xl"
       >
         <TbArrowDownRhombus />
       </a>
