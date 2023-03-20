c4c28ef951814b603ba09f613c7bc228c513e897
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-20 00:28:26 -0400

    feat: implement House page on 2xl

--- app/houses/[name]/components/TopSection.tsx
+++ app/houses/[name]/components/TopSection.tsx
@@ -10,13 +10,13 @@ interface TopSectionProps {
 export default function TopSection({ houseName, id }: TopSectionProps) {
   return (
     <section className="h-screen">
-      <h2 className="pt-28 text-center font-cardo text-9xl">
+      <h2 className="pt-36 text-center font-cardo text-9xl">
         House {houseName}
       </h2>
       <Image
         src={housesImages[+id - 1]}
         alt={houseName}
-        className="mx-auto mt-12 h-3/5 w-auto"
+        className="mx-auto mt-14 h-3/5 w-auto"
       />
     </section>
   );
