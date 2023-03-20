9aa0cd67e1226946713cc68667fd768cd28cce70
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-20 01:28:46 -0400

    feat: implement House page on sm

--- app/houses/[name]/components/Card.tsx
+++ app/houses/[name]/components/Card.tsx
@@ -14,29 +14,29 @@ export default function Card({
   titles,
 }: CardProps) {
   return (
-    <section className="mx-auto mt-4 grid w-4/6 grid-cols-card items-center justify-items-center gap-y-4 rounded-md bg-black/40 py-2 px-6 text-center text-base sm:mt-6 sm:py-4 sm:px-8 sm:text-lg md:mt-8 md:gap-y-6 md:py-6 md:px-10 md:text-xl lg:mt-10 lg:gap-y-8 lg:py-8 lg:px-12 lg:text-2xl xl:mt-12 xl:gap-y-10 xl:py-10 xl:px-14 xl:text-3xl 2xl:mt-14 2xl:text-4xl">
-      <div className="flex w-full border-b-4 border-stone-600 pb-1 text-zinc-400 sm:pb-1.5 md:pb-2 lg:pb-4">
+    <section className="mx-auto mt-4 grid grid-cols-card items-center justify-items-center gap-y-4 rounded-md bg-black/40 py-2 px-6 text-center text-lg sm:mt-8 sm:w-8.5/10 sm:gap-y-6 sm:py-6 sm:px-8 sm:text-xl md:w-4/6 md:px-10 lg:mt-10 lg:gap-y-8 lg:py-8 lg:px-12 lg:text-2xl xl:mt-12 xl:gap-y-10 xl:py-10 xl:px-14 xl:text-3xl 2xl:mt-14 2xl:text-4xl">
+      <div className="flex w-full border-b-4 border-stone-600 pb-1 text-zinc-400 sm:pb-2 lg:pb-4">
         <span className="">Seat</span>
       </div>
-      <span className="w-full border-b-4 border-stone-600 pb-1 sm:pb-1.5 md:pb-2 lg:pb-4">
+      <span className="w-full border-b-4 border-stone-600 pb-1 sm:pb-2 lg:pb-4">
         {seat}
       </span>
-      <div className="flex w-full border-b-4 border-stone-600 pb-1 text-zinc-400 sm:pb-1.5 md:pb-2 lg:pb-4">
+      <div className="flex w-full border-b-4 border-stone-600 pb-1 text-zinc-400 sm:pb-2 lg:pb-4">
         <span className="">Region</span>
       </div>
-      <div className="w-full border-b-4 border-stone-600 pb-1 sm:pb-1.5 md:pb-2 lg:pb-4">
+      <div className="w-full border-b-4 border-stone-600 pb-1 sm:pb-2 lg:pb-4">
         {region}
       </div>
-      <div className="flex w-full border-b-4 border-stone-600 pb-1 text-zinc-400 sm:pb-1.5 md:pb-2 lg:pb-4">
+      <div className="flex w-full border-b-4 border-stone-600 pb-1 text-zinc-400 sm:pb-2 lg:pb-4">
         <span className="">Words</span>
       </div>
-      <span className="w-full border-b-4 border-stone-600 pb-1 sm:pb-1.5 md:pb-2 lg:pb-4">
+      <span className="w-full border-b-4 border-stone-600 pb-1 sm:pb-2 lg:pb-4">
         &quot;{words}&quot;
       </span>
-      <div className="flex w-full border-b-4 border-stone-600 pb-1 text-zinc-400 sm:pb-1.5 md:pb-2 lg:pb-4">
+      <div className="flex w-full border-b-4 border-stone-600 pb-1 text-zinc-400 sm:pb-2 lg:pb-4">
         <span className="">Religion</span>
       </div>
-      <span className="w-full border-b-4 border-stone-600 pb-1 sm:pb-1.5 md:pb-2 lg:pb-4">
+      <span className="w-full border-b-4 border-stone-600 pb-1 sm:pb-2 lg:pb-4">
         {religion}
       </span>
       <div className="flex h-full w-full text-zinc-400">
@@ -44,7 +44,7 @@ export default function Card({
           {titles.length > 2 ? 'Titles' : 'Title'}
         </span>
       </div>
-      <div className="flex w-full flex-col text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
+      <div className="flex w-full flex-col text-lg sm:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl">
         {titles.map((title, i) => (
           <span key={i}>{title}</span>
         ))}
--- app/houses/[name]/components/Description.tsx
+++ app/houses/[name]/components/Description.tsx
@@ -6,8 +6,8 @@ interface DescriptionProps {
 
 export default function Description({ description }: DescriptionProps) {
   return (
-    <section className="mx-auto mt-12 w-10/12 sm:mt-14 md:mt-16 lg:mt-20 xl:mt-24 2xl:mt-28">
-      <h3 className="mb-1 text-center text-3xl sm:text-4xl md:mb-1.5 md:text-5xl lg:mb-4 lg:text-6xl xl:mb-6 xl:text-7xl 2xl:mb-8 2xl:text-8xl">
+    <section className="mx-auto mt-12 w-11/12 sm:mt-14 md:mt-16 lg:mt-20 lg:w-10/12 xl:mt-24 2xl:mt-28">
+      <h3 className="mb-1 text-center text-4xl sm:text-5xl md:mb-1.5 lg:mb-4 lg:text-6xl xl:mb-6 xl:text-7xl 2xl:mb-8 2xl:text-8xl">
         Description
       </h3>
       {description.map((par, i) => (
@@ -15,8 +15,8 @@ export default function Description({ description }: DescriptionProps) {
           key={i}
           className={
             i !== 0
-              ? 'mb-1.5 text-sm sm:mb-2 sm:text-base md:mb-3 md:text-lg lg:mb-4 lg:text-xl xl:mb-6 xl:text-2xl 2xl:mb-8 2xl:text-3xl'
-              : 'mb-1.5 text-sm first-letter:font-cardo first-letter:text-3xl first-letter:leading-3 sm:mb-2 sm:text-base sm:first-letter:text-4xl md:mb-3 md:text-lg md:first-letter:text-5xl lg:mb-4 lg:text-xl lg:first-letter:text-6xl xl:mb-6 xl:text-2xl xl:first-letter:text-6xl 2xl:mb-8 2xl:text-3xl 2xl:first-letter:text-7xl'
+              ? 'mb-3 text-base md:text-lg lg:mb-4 lg:text-xl xl:mb-6 xl:text-2xl 2xl:mb-8 2xl:text-3xl'
+              : 'mb-3 text-base first-letter:font-cardo first-letter:text-3xl first-letter:leading-3 sm:first-letter:text-4xl md:text-lg md:first-letter:text-5xl lg:mb-4 lg:text-xl lg:first-letter:text-6xl xl:mb-6 xl:text-2xl xl:first-letter:text-6xl 2xl:mb-8 2xl:text-3xl 2xl:first-letter:text-7xl'
           }
         >
           {par}
@@ -24,7 +24,7 @@ export default function Description({ description }: DescriptionProps) {
       ))}
       <Link
         href="/houses"
-        className="mx-auto mt-8 mb-6 block w-fit rounded-md bg-zinc-700 py-1 px-2 text-base duration-300 hover:bg-orange-600 sm:mt-10 sm:mb-8 sm:py-1.5 sm:px-3 sm:text-lg md:mt-12 md:mb-10 md:py-3 md:px-6 md:text-2xl lg:mt-16 lg:mb-14 xl:mt-24 xl:mb-16 xl:py-4 xl:px-8 xl:text-3xl 2xl:mt-32 2xl:mb-20 2xl:py-5 2xl:px-10 2xl:text-4xl"
+        className="mx-auto mt-8 mb-6 block w-fit rounded-md bg-zinc-700 py-1 px-2 text-base duration-300 hover:bg-orange-600 sm:mt-10 sm:mb-8 sm:py-1.5 sm:px-4 sm:text-xl md:mt-12 md:mb-10 md:py-3 md:px-6 md:text-2xl lg:mt-16 lg:mb-14 xl:mt-24 xl:mb-16 xl:py-4 xl:px-8 xl:text-3xl 2xl:mt-32 2xl:mb-20 2xl:py-5 2xl:px-10 2xl:text-4xl"
       >
         Back to Houses
       </Link>
