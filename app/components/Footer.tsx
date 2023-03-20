116aa41c89aeff31cd7180538c8a1fb5554117a6
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-20 08:04:40 -0400

    feat: add custom focus stules for footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,18 +4,18 @@ export default function Footer() {
   return (
     <footer className="w-screen bg-black shadow-xl">
       <ul className="mx-auto flex w-11/12 flex-col justify-around gap-y-5 py-7 text-base sm:flex-row sm:py-12 md:py-20 md:text-xl 2xl:py-24 2xl:text-2xl">
-        <li>
+        <li className="relative">
           <a
             href="mailto:spievakov@gamil.com"
-            className="block text-center duration-300 hover:text-orange-600"
+            className="block text-center outline-none duration-300 hover:text-orange-600 focus:after:absolute focus:after:top-8 focus:after:block focus:after:h-0.5 focus:after:w-full focus:after:bg-orange-600 lg:focus:after:top-9 2xl:focus:after:top-11 2xl:focus:after:h-1"
           >
             spievakov@gmail.com
           </a>
         </li>
-        <li>
+        <li className="relative">
           <a
             href="tel:+16474739502"
-            className="block text-center duration-300 hover:text-orange-600"
+            className="block text-center outline-none duration-300 hover:text-orange-600 focus:after:absolute focus:after:top-8 focus:after:block focus:after:h-0.5 focus:after:w-full focus:after:bg-orange-600 lg:focus:after:top-9 2xl:focus:after:top-11 2xl:focus:after:h-1"
           >
             +1 (647) 473-9502
           </a>
@@ -25,7 +25,7 @@ export default function Footer() {
             href="https://github.com/AlexandrSpevakov/got-explorer"
             target="_blank"
             rel="noreferrer"
-            className="duration-300 hover:text-orange-600"
+            className="rounded outline-none duration-300 hover:text-orange-600 focus:outline-offset-4 focus:outline-orange-600"
             aria-label="Github link"
           >
             <BsGithub />
@@ -34,7 +34,7 @@ export default function Footer() {
             href="https://www.linkedin.com/in/oleksandr-spievakov/"
             target="_blank"
             rel="noreferrer"
-            className="duration-300 hover:text-orange-600"
+            className="rounded outline-none duration-300 hover:text-orange-600 focus:outline-offset-4 focus:outline-orange-600"
             aria-label="Linkedin link"
           >
             <BsLinkedin />
@@ -43,7 +43,7 @@ export default function Footer() {
             href="https://www.instagram.com/_spevakov_/"
             target="_blank"
             rel="noreferrer"
-            className="duration-300 hover:text-orange-600"
+            className="rounded outline-none duration-300 hover:text-orange-600 focus:outline-offset-4 focus:outline-orange-600"
             aria-label="Instagram link"
           >
             <BsInstagram />
