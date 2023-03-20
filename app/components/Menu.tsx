79d83cafdd11006c94dc1004f696cbad7fcf8bc1
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-20 05:55:36 -0400

    feat: costomize menu links focus styling

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -8,40 +8,40 @@ export default function Menu() {
 
   return (
     <ul className="hidden gap-x-5 text-xl md:flex lg:gap-x-8 lg:text-2xl 2xl:gap-x-12 2xl:text-3xl">
-      <li>
+      <li className="relative">
         <Link
           href="/"
-          className={`duration-300 ${
+          className={`outline-none duration-300 focus:after:absolute focus:after:top-8 focus:after:block focus:after:h-0.5 focus:after:w-full focus:after:bg-orange-600 lg:focus:after:top-9 2xl:focus:after:top-11 2xl:focus:after:h-1 ${
             path === '/' ? 'text-orange-600' : 'hover:text-orange-600'
           }`}
         >
           Home
         </Link>
       </li>
-      <li>
+      <li className="relative">
         <Link
           href="/houses"
-          className={`duration-300 ${
+          className={`outline-none duration-300 focus:after:absolute focus:after:top-8 focus:after:block focus:after:h-0.5 focus:after:w-full focus:after:bg-orange-600 lg:focus:after:top-9 2xl:focus:after:top-11 2xl:focus:after:h-1 ${
             path === '/houses' ? 'text-orange-600' : 'hover:text-orange-600'
           }`}
         >
           Houses
         </Link>
       </li>
-      <li>
+      <li className="relative">
         <Link
           href="/characters"
-          className={`duration-300 ${
+          className={`outline-none duration-300 focus:after:absolute focus:after:top-8 focus:after:block focus:after:h-0.5 focus:after:w-full focus:after:bg-orange-600 lg:focus:after:top-9 2xl:focus:after:top-11 2xl:focus:after:h-1 ${
             path === '/characters' ? 'text-orange-600' : 'hover:text-orange-600'
           }`}
         >
           Characters
         </Link>
       </li>
-      <li>
+      <li className="relative">
         <Link
           href="/dragons"
-          className={`duration-300 ${
+          className={`outline-none duration-300 focus:after:absolute focus:after:top-8 focus:after:block focus:after:h-0.5 focus:after:w-full focus:after:bg-orange-600 lg:focus:after:top-9 2xl:focus:after:top-11 2xl:focus:after:h-1 ${
             path === '/dragons' ? 'text-orange-600' : 'hover:text-orange-600'
           }`}
         >
