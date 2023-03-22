3066c44721fbf056ec58eb76de4afebd6b38ad8e
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-21 20:28:49 -0400

    fix: fix not found message logic

--- app/characters/components/CharactersGrid.tsx
+++ app/characters/components/CharactersGrid.tsx
@@ -23,7 +23,8 @@ export default function CharactersGrid({
   return (
     <section
       className={
-        searchValue.length >= 1 && filteredCharacters.length < 1
+        (nameValue.length >= 1 || titleValue.length >= 1) &&
+        filteredCharacters.length < 1
           ? 'mx-auto mt-16 2xl:mt-24'
           : 'characters-grid mx-auto mt-10 grid justify-center gap-y-6 sm:mt-10 sm:gap-8 sm:px-0 md:gap-10 lg:mt-12 lg:gap-x-6 lg:gap-y-12 xl:mt-16 xl:gap-x-8 xl:gap-y-14 2xl:mt-24 2xl:gap-y-16 2xl:gap-x-10'
       }
