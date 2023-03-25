07875135910b8ca89cef61b88747ee223fd721fd
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-25 02:33:11 -0400

    feat: add 6 first dragon images

---
+++ app/dragons/data/dragonsImages.ts
@@ -0,0 +1,15 @@
+import balerion from '../../../public/dragons/balerion.webp';
+import arrax from '../../../public/dragons/arrax.webp';
+import cannibal from '../../../public/dragons/cannibal.webp';
+import caraxes from '../../../public/dragons/caraxes.webp';
+import dreamfyre from '../../../public/dragons/dreamfyre.webp';
+import drogon from '../../../public/dragons/drogon.webp';
+
+export const dragonsImages = [
+  arrax,
+  balerion,
+  cannibal,
+  caraxes,
+  dreamfyre,
+  drogon,
+];
