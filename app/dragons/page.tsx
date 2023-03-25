15e2c2067a77d9cb8fbbdef400ad99cbf599457d
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-25 09:38:10 -0400

    refactor: disable dragons pages

--- app/dragons/page.tsx
+++ app/dragons/page.tsx
@@ -13,31 +13,35 @@ export const metadata = {
   ],
 };
 
-async function getDragons() {
-  const data = await fetch(
-    'https://641ef7ddf228f1a83eadac82.mockapi.io/dragons',
-  );
-  const dragons: IDragon[] = await data.json();
-  return dragons;
-}
+// async function getDragons() {
+//   const data = await fetch(
+//     'https://641ef7ddf228f1a83eadac82.mockapi.io/dragons',
+//   );
+//   const dragons: IDragon[] = await data.json();
+//   return dragons;
+// }
+
+// export default async function Dragons() {
+//   const dragons: IDragon[] = await getDragons();
+
+//   return (
+//     <main>
+//       <section className="m-auto mb-32 w-11/12">
+//         <div
+//           className="absolute top-0 left-0 z-[-1] h-screen w-screen bg-cover bg-center bg-no-repeat opacity-10"
+//           style={{
+//             backgroundImage: `url(${heroBg.src})`,
+//           }}
+//         />
+//         <h2 className="pt-32 text-center font-cardo text-5xl sm:text-6xl md:text-7xl lg:pt-40 lg:text-8xl xl:text-9xl 2xl:pt-52">
+//           Dragons
+//         </h2>
+//         <MainContent dragons={dragons} />
+//       </section>
+//     </main>
+//   );
+// }
 
 export default async function Dragons() {
-  const dragons: IDragon[] = await getDragons();
-
-  return (
-    <main>
-      <section className="m-auto mb-32 w-11/12">
-        <div
-          className="absolute top-0 left-0 z-[-1] h-screen w-screen bg-cover bg-center bg-no-repeat opacity-10"
-          style={{
-            backgroundImage: `url(${heroBg.src})`,
-          }}
-        />
-        <h2 className="pt-32 text-center font-cardo text-5xl sm:text-6xl md:text-7xl lg:pt-40 lg:text-8xl xl:text-9xl 2xl:pt-52">
-          Dragons
-        </h2>
-        <MainContent dragons={dragons} />
-      </section>
-    </main>
-  );
+  return null;
 }
